c10a82a9f5df720f3b6b4e84a0de20484bf4b80c
Ami Paradkar <amimehta78@gmail.com>
2015-10-02 17:44:06 -0400

    App User added functanatlity

--- src/main/java/com/iRentService/controller/AppUserController.java
+++ src/main/java/com/iRentService/controller/AppUserController.java
@@ -14,22 +14,49 @@ import com.iRentService.repository.AppUserRepository;
 @RestController
 public class AppUserController {
 
-		private final AppUserRepository appUserRepository ;
+	private final AppUserRepository appUserRepository;
 
-	    @RequestMapping(value = "/iRentService/user/{userFirstName}", method = RequestMethod.GET)
-	    AppUser getDetails(@PathVariable String userFirstName){	    	
-	    	System.out.println("USERID == "+userFirstName);
-	    	return this.appUserRepository.findByFirstName(userFirstName);
-	    }
-	    @RequestMapping(value = "/iRentService/user/all", method = RequestMethod.GET)
-	    List<AppUser> getDetails(){
-	    
-	    	return this.appUserRepository.findAll();
-	    	
-	    }
-	    @Autowired
-	    AppUserController(AppUserRepository appUserRepository) {
-			this.appUserRepository = appUserRepository;
-		}
-	    
+//	@RequestMapping(value = "/user", method = RequestMethod.GET)
+//	public Principal user(Principal user) {
+//		return user;
+//	}
+
+	@RequestMapping(value = "/iRentService/user/{userFirstName}", method = RequestMethod.GET)
+	AppUser getDetailByName(@PathVariable String userFirstName) {
+		System.out.println("USERName == " + userFirstName);
+		return this.appUserRepository.findByFirstName(userFirstName);
+	}
+//	@RequestMapping(value = "/iRentService/user/{symbolicName:[/d]}", method = RequestMethod.GET)
+//	AppUser getDetailById(@PathVariable Long userId) {
+//		
+//		System.out.println("USERID == " + userId);
+//		return this.appUserRepository.findById(userId);
+//	}
+
+	@RequestMapping(value = "/iRentService/user/all", method = RequestMethod.GET)
+	List<AppUser> getDetails() {
+		return this.appUserRepository.findAll();
+	}
+	@RequestMapping(value = "/iRentService/user/create", method = RequestMethod.POST)
+	void saveAppUser() {
+
+		 //this.appUserRepository.save(appUser);
+
+	}
+
+//	@Configuration
+//	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/")
+//					.permitAll().anyRequest().authenticated();
+//		}
+//	}
+
+	@Autowired
+	AppUserController(AppUserRepository appUserRepository) {
+		this.appUserRepository = appUserRepository;
+	}
+
 }
